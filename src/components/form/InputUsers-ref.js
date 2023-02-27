1f42aea296d98a54944f7c2ade1a9445d50c5ec5
Nunsinee Viken <71260513+nunsinee@users.noreply.github.com>
2023-02-27 03:18:34 +0100

    update InputUser

--- src/components/form/InputUsers-ref.js
+++ src/components/form/InputUsers-ref.js
@@ -1,27 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import styles from "./InputUsers.module.css";
 import Button from "../ui/Button";
 
 import Card from "../ui/Card";
 import ErrorModal from "../ui/ErrorModal";
 
+// import Wrapper from "../helpers/Wrapper";
+
 const InputUsers = (props) => {
-	const [enteredName, setEnteredName] = useState("");
-	const [enteredAge, setEnteredAge] = useState("");
+	/* Reading value from DOM (Input) useRef insted of useState is better solution
+	useState is clear code but code will be longer*/
+
+	const nameInputRef = useRef();
+	const ageInputRef = useRef();
+
+	// const [enteredName, setEnteredName] = useState("");
+	// const [enteredAge, setEnteredAge] = useState("");
 	const [error, setError] = useState();
 
-	const enteredNameValue = (event) => {
-		setEnteredName(event.target.value);
-	};
+	// const enteredNameValue = (event) => {
+	// 	setEnteredName(event.target.value);
+	// };
 
-	const enteredAgeValue = (event) => {
-		setEnteredAge(event.target.value);
-	};
+	// const enteredAgeValue = (event) => {
+	// 	setEnteredAge(event.target.value);
+	// };
 
 	const submitHandlers = (event) => {
 		event.preventDefault();
+		const enteredUserName = nameInputRef.current.value;
+		const enteredUserAge = ageInputRef.current.value;
 
-		if (enteredName.trim().length === 0 || enteredAge.trim().length === 0) {
+		if (
+			enteredUserName.trim().length === 0 ||
+			enteredUserAge.trim().length === 0
+		) {
 			setError({
 				title: "Invalid Input",
 				message: "Please enter a valid name and age (non-empty value)",
@@ -29,7 +42,7 @@ const InputUsers = (props) => {
 			return;
 		}
 
-		if (+enteredAge < 1) {
+		if (+enteredUserAge < 1) {
 			setError({
 				title: "Invalid Age",
 				message: "Please enter a valid age ( > 0)",
@@ -44,15 +57,14 @@ const InputUsers = (props) => {
 		// 	id: Math.random().toString(),
 		// };
 
-		console.log(enteredName, enteredAge);
+		// console.log(enteredName, enteredAge);
 
 		// Alternative 2
 		// props.onSaveUsers(usersData);
 
-		props.onSaveUsers(enteredName, enteredAge);
-
-		setEnteredName("");
-		setEnteredAge("");
+		props.onSaveUsers(enteredUserName, enteredUserAge);
+		nameInputRef.current.value = "";
+		ageInputRef.current.value = "";
 	};
 
 	const errorHandler = () => {
@@ -60,7 +72,7 @@ const InputUsers = (props) => {
 	};
 
 	return (
-		<>
+		<React.Fragment>
 			{error && (
 				<ErrorModal
 					title={error.title}
@@ -75,8 +87,9 @@ const InputUsers = (props) => {
 						<input
 							type="text"
 							id="name"
-							value={enteredName}
-							onChange={enteredNameValue}
+							// value={enteredName}
+							// onChange={enteredNameValue}
+							ref={nameInputRef}
 						/>
 					</div>
 					<div className={styles["form-control"]}>
@@ -84,14 +97,15 @@ const InputUsers = (props) => {
 						<input
 							type="number"
 							id="age"
-							value={enteredAge}
-							onChange={enteredAgeValue}
+							// value={enteredAge}
+							// onChange={enteredAgeValue}
+							ref={ageInputRef}
 						/>
 					</div>
 					<Button type="submit">Add new user</Button>
 				</form>
 			</Card>
-		</>
+		</React.Fragment>
 	);
 };
 
